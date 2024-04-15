435758d6beaa9e42ba1616538b503e37939ac69a
Jonathan Maronde <j2mike4@gmail.com>
2024-04-15 16:25:02 -0500

    Added a view and url for the About page.

--- devportfolio/devportfolio/urls.py
+++ devportfolio/devportfolio/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home),
     path("", RedirectView.as_view(url="home/", permanent=True)),
-]
+    path('about/', views.about)
+]  
